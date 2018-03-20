5a1edea3119081829c533a22bbd9a5a88086a595
donkeypagoda <christopherallenstewart@gmail.com>
2018-03-19 17:40:43 -0700

    building state and such

--- src/App.js
+++ src/App.js
@@ -2,12 +2,51 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function ListPosts(props){
+  return (
+    <div>
+      <h2> {props.title} </h2>
+    </div>
+  )
+}
+
+
+
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      postList: [
+        {
+          timestamp: '2012-02-18 14:28:32',
+          title: "I ate the berries",
+          content: "berries are so good and yummy and I like them because I am a deer"
+        },
+        {
+          timestamp: '2012-02-20 16:28:32',
+          title: "There is a mean bear named Rodzher in the forest",
+          content: "watch out cause if he catches you he will make you listen to all of his puns and also he likes venison"
+        },
+        {
+          timestamp: '2014-02-25 12:28:32',
+          title: "Do deer like tacos?",
+          content: "of course they do, everyone loves tacos what a silly question, what are you a bear???"
+        }
+      ]
+    }
+  }
   render() {
+    let postList = []
+    this.state.postList.forEach( post => {
+      postList.push(<ListPosts post={post} />)
+    })
     return (
       <div className="App">
         <h1>DeerDiary</h1>
         <h4> - a diary for deer</h4>
+        <div>
+          {postList}
+        </div>
       </div>
     );
   }
