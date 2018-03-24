d4bd311264e5a949da158b6d8166e584945fb931
donkeypagoda <christopherallenstewart@gmail.com>
2018-03-24 11:56:29 -0700

    format

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import NewPost from './components/newPost'
 function ListPosts(props){
   return (
     <div className='border'>
+    <div>
       <h2> {props.title} </h2>
+      <h4> {props.date} </h4>
+    </div>
       <p> {props.content}</p>
     </div>
   )
@@ -19,16 +22,19 @@ class App extends Component {
     this.state = {
       postList: [
         {
+          id: 1,
           timestamp: '2012-02-18 14:28:32',
           title: "I ate the berries",
           content: "berries are so good and yummy and I like them because I am a deer"
         },
         {
+          id: 2,
           timestamp: '2012-02-20 16:28:32',
           title: "There is a bear named Rodzher in the forest",
           content: "watch out cause if he catches you he will make you listen to all of his puns and also he likes venison"
         },
         {
+          id: 3,
           timestamp: '2014-02-25 12:28:32',
           title: "Do deer like tacos?",
           content: "of course they do, everyone loves tacos what a silly question, what are you a bear???"
@@ -46,7 +52,7 @@ class App extends Component {
   render() {
     let postList = []
     this.state.postList.forEach( post => {
-      postList.push(<ListPosts title={post.title} content={post.content} />)
+      postList.push(<ListPosts key={post.id} title={post.title} content={post.content} date={post.timestamp}/>)
     })
     let showForm = []
     if (this.state.postForm){
