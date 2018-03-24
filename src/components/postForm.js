b95e50e1ab5f9bd3a37a6ef2642b8827642d40b9
donkeypagoda <christopherallenstewart@gmail.com>
2018-03-24 13:18:21 -0700

    fixing form up

--- src/components/postForm.js
+++ src/components/postForm.js
@@ -1,10 +1,27 @@
 import React from 'react'
 
 const PostForm = (state, props) => {
+  const submitPost = (e) => {
+    e.preventDefault()
+    console.log(e.target)
+  }
   return (
-    <form>
-      HAYO
-    </form>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <form name="newBlogPost" onSubmit={submitPost}>
+            <div className="form-group">
+              <label>Entry Title</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="newPostTitle"
+                />
+            </div>
+          </form>
+        </div>
+      </div>
+      </div>
   )
 }
 
