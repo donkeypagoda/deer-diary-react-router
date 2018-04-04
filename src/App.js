aef0533d2ec1a59dc018001851c4a2cd5bd29519
donkeypagoda <christopherallenstewart@gmail.com>
2018-04-04 14:47:34 -0700

    setting up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import './App.css';
 import PostForm from './components/postForm'
 import moment from 'moment'
