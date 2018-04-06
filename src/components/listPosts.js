742813f17588a3de5d0d6d37f8fe0f22b66c9460
donkeypagoda <christopherallenstewart@gmail.com>
2018-04-06 13:44:29 -0700

    import export

--- src/components/listPosts.js
+++ src/components/listPosts.js
@@ -1,23 +1,30 @@
 import React, {Component} from 'react'
-import{Link} from 'react-router-dom'
+import {Link} from 'react-router-dom'
+
+class ListPosts extends Component{
 
 
-
-function ListPosts(props){
-  return (
-    <div className='list-group'>
-      <div className="list-group-item flex-column align-items-start">
-        <div className="d-flex w-100 justify-content-between">
-          <h5 className="mb-1">
-            {props.title}
-          </h5>
-          <small>{moment(props.date).format("MMM Do YY")}</small>
+  render(){
+    return (
+      <div className='list-group'>
+        <div className="list-group-item flex-column align-items-start">
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">
+              {props.title}
+            </h5>
+            <small>{moment(props.date).format("MMM Do YY")}</small>
+          </div>
+          <div className="mb-1">
+            {props.content}
+            </div>
+          <button type="button" className="btn btn-secondary" onClick={() => props.action(props.id)}>Delete Post</button>
         </div>
-        <div className="mb-1">
-          {props.content}
-        </div>
-        <button type="button" className="btn btn-secondary" onClick={() => props.action(props.id)}>Delete Post</button>
       </div>
-    </div>
-  )
+    )
+  }
 }
+
+
+
+
+export default ListPosts
