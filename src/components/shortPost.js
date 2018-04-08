4905701fae238c2d06e60025fd756d4f1ee7c789
donkeypagoda <christopherallenstewart@gmail.com>
2018-04-08 13:06:09 -0700

    changing architecture to add truncated posts before implementing router fully

---
+++ src/components/shortPost.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import Link from 'react-router-dom'
+import moment from 'moment'
+
+class ShortPost extends Component{
+  render(){
+    return(
+      
+    )
+  }
+}
+
+export default ShortPost
