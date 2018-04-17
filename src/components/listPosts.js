5ed2fcd391339c3e95cf9a062e89f3d53f71632f
donkeypagoda <christopherallenstewart@gmail.com>
2018-04-17 12:14:40 -0700

    removing unused variables and cleaning up react errors

--- src/components/listPosts.js
+++ src/components/listPosts.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
-import moment from 'moment'
 import ShortPost from './shortPost.js'
 
 class ListPosts extends Component{
