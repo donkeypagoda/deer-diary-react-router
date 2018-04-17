8bf3db076743c14b32d6a17a6af3681fc7a00770
donkeypagoda <christopherallenstewart@gmail.com>
2018-04-17 12:17:33 -0700

    all routes work, all cleaned up, might be ready for redux

--- src/components/singlePost.js
+++ src/components/singlePost.js
@@ -36,18 +36,27 @@ class SinglePost extends Component {
   render(){
     if (this.state.post === undefined) return <div>Loading...</div>
     return(
-      <div className='list-group'>
-        <div className="list-group-item flex-column align-items-start">
-          <div className="d-flex w-100 justify-content-between">
-            <h5 className="mb-1">
-              {this.state.post.title}
-            </h5>
-            <small>{moment(this.state.post.created_at).format("MMM Do YY")}</small>
+      <div className="container">
+        <div className="row lift">
+          <div className="col-md-12">
+            <div className='btn btn-secondary'>
+              <Link to='/'>Return to Posts</Link>
+            </div>
           </div>
-          <div className="mb-1">
-            {this.state.post.content}
+        </div>
+        <div className='list-group'>
+          <div className="list-group-item flex-column align-items-start">
+            <div className="d-flex w-100 justify-content-between">
+              <h5 className="mb-1">
+                {this.state.post.title}
+              </h5>
+              <small>{moment(this.state.post.created_at).format("MMM Do YY")}</small>
+            </div>
+            <div className="mb-1">
+              {this.state.post.content}
+            </div>
+            <button type="button" className="btn btn-secondary" onClick={() => this.deletePost(this.state.post.id)}>Delete Post</button>
           </div>
-          <button type="button" className="btn btn-secondary" onClick={() => this.deletePost(this.state.post.id)}>Delete Post</button>
         </div>
       </div>
     )
